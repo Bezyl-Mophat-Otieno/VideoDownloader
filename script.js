0f341a95f0f1aecc2d5e49e8dc3f8c7ff927064d
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2022-12-20 18:30:09 +0300

    Completed .
    The simple web App enables a person download any type of  files at a click of a button.

--- script.js
+++ script.js
@@ -15,18 +15,22 @@ const fetchFile = (url)=>{
     //fetching file and returning response as blob
     fetch(url).then(res=>res.blob()).then(file=>{
         //Creating a temporary URL from the passed in file 
-        const tempURL =  URL.createObjectURL(file);
+
+        const tempURL =  URL.createObjectURL(file);        
         const aTag = document.createElement("a");
         //Passing temp URL as the href attribute of element <a>
         aTag.href=tempURL;
         aTag.download = url.replace(/^.*[\\\/]/,'');
-        document.appendChild(file);
+        console.log(aTag)
+        document.body.appendChild(aTag);
+        
         aTag.click();
         aTag.remove();// Remove a Tag after file download
         URL.revokeObjectURL(tempURL)//Removing temp URL after file download
         button.innerText("Downloading ...");
     }).catch(
 ()=>{
+    
 button.innerText="Download File";
     alert("Failed to Download The File");
 }
